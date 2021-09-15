228bd0b9f929d6e8385f92e33382803ebb30fc3c
anjali1102 <ac.chauhan1102@gmail.com>
2021-09-15 19:49:47 +0530

    Added API calls

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+
 var outputDiv = document.querySelector("#output");
 
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+  return serverURL+"?"+"text="+text
+}
+
+function errorHandler(error){
+  console.log("error occured", error);
+  alert("Server Down , Try again after sometime ")
+}
+
 function clickHandler() {
-  outputDiv.innerHTML = "Transalting to bananaaa......"+ txtInput.value;
+  var inputText=txtInput.value;
+
+  //calling server for processing
+  fetch(getTranslationURL(inputText)) 
+  .then(response=>response.json())
+  .then(json=>{
+    var translatedText=json.contents.translated;
+    outputDiv.innerText=translatedText;
+  })
+  .catch(errorHandler)
 };
 btnTranslate.addEventListener("click", clickHandler);
